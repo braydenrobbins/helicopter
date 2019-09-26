c8f4949e5d0608594263efe93cd2ca0174e0d428
Brayden Robbins <brayden.robbins@ruralsource.com>
2019-09-26 08:13:29 -0500

    added a popover to each card that lets the user choose to edit or delete a heli

--- src/Helicopter/helicopter-card.js
+++ src/Helicopter/helicopter-card.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import { Card } from 'antd';
+import React, { useState } from 'react';
+import { Card, Popover, Button } from 'antd';
 
 const { Meta } = Card;
 
 const HelicopterCard = (props) => {
+  const [visible, setVisible] = useState(false);
+
+  function hide() {
+    setVisible(false);
+  }
+
+  function showPopover() {
+    setVisible(true);
+  }
+
+  const popoverButtons = (
+    <>
+      <Button onClick={editHeli} type='primary'>Edit</Button>
+      <Button onClick={deleteHeli} type='danger'>Delete</Button>
+    </>
+  );
+
+  function editHeli() {
+    console.log('edit');
+    hide()
+  }
+
+  function deleteHeli() {
+    console.log('delete');
+    hide();
+  }
   return (
-    <Card
-      hoverable
-      className='helicopter-card'
-      cover={<img alt="example" src={props.helicopter.src} className='helicopter-Img' />}
-    >
-      <Meta title={props.helicopter.name} description={props.helicopter.date} />
-    </Card>
+    <Popover content={popoverButtons} title="Options" trigger="click" visible={visible} >
+      <Card
+        hoverable
+        className='helicopter-card'
+        cover={<img alt="example" src={props.helicopter.src} className='helicopter-Img' />}
+        onClick={showPopover}
+      >
+        <Meta title={props.helicopter.name} description={props.helicopter.date} />
+      </Card>
+    </Popover>
   )
 }
 
